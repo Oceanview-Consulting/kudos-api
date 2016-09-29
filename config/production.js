cedc01085e702677ef45cae5e3a2099e588bd285
travisstokes <travis@sysgineer.com>
2016-09-28 20:09:16 -0400

    Minor tweak

--- config/production.js
+++ config/production.js
@@ -1,3 +1,3 @@
 module.exports = {
-    "connectionString": "postgres://nytooihkxbeczf:G7muKvXqOIvtqGBdk-ey-UCzPD@ec2-54-243-249-65.compute-1.amazonaws.com:5432/daqo1jf9rr41ha
+    "connectionString": "postgres://nytooihkxbeczf:G7muKvXqOIvtqGBdk-ey-UCzPD@ec2-54-243-249-65.compute-1.amazonaws.com:5432/daqo1jf9rr41ha"
 }
